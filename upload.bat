2d003d1627a5da05438e9eb32e06c78ecaaeb0dc
undefined06855 <currentcashew57@gmail.com>
2023-02-08 21:41:55 +0000

    1.11

--- upload.bat
+++ upload.bat
@@ -4,5 +4,6 @@ git init
 git add .
 git commit -m %version%
 git remote add origin https://github.com/undefined06855/Clock.git
+git update-index --assume-unchanged upload.bat
 git remote -v
 git push origin master --force
