ab214ab180a365c244a738c1e9047967ac443436
undefined06855 <currentcashew57@gmail.com>
2023-02-09 23:39:16 +0000

    1.24

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,15 @@
-// "IT IS_FIVETENFIFTEENTWENTY-FIVE_MINUTESHALF_PASTTO_ONETWOTHREEFOURFIVESIXSEVENEIGHTNINETENELEVENTWELVE_PMAM."
-// length: 108
-// width: 9
-
 var d = new Date()
 var t = [] // display time
 
 // debug
-const DEBUG = false
-const hour = 5
-const minute = 25
+const DEBUG = new URL(document.location).searchParams.get("debug")
+var hour = 22
+var minute = 58
 
-if (DEBUG)
+if (DEBUG === "")
 {
     d.getHours = () => {return hour}
     d.getMinutes = () => {return minute}
-    d.getSeconds = () => {return 0}
     console.log(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
 }
 
@@ -22,7 +17,7 @@ if (DEBUG)
 // https://stackoverflow.com/a/19277804
 function findNearest(input)
 {
-    return [0, 5, 10, 15, 20, 25].reduce(function(prev, curr) {
+    return [5, 10, 15, 20, 25].reduce(function(prev, curr) {
         return (Math.abs(curr - input) < Math.abs(prev - input) ? curr : prev);
     })
 }
@@ -36,26 +31,56 @@ function convertTo12Hour(hour)
     throw("Error when converting to 12 hour formet: ", hour, " is not valid!")
 }
 
-if (findNearest(d.getMinutes()) == 0) t = ["", convertTo12Hour(d.getHours())[0], convertTo12Hour(d.getHours())[1]]
-else if (d.getMinutes() < 28) t = [findNearest(d.getMinutes()), "minutes", "past", convertTo12Hour(d.getHours())[0], convertTo12Hour(d.getHours())[1]]
-else if (d.getMinutes() > 32) t = [findNearest(60 - d.getMinutes()), "minutes", "to", d.getHours == 23 ? "midnight" : convertTo12Hour(d.getHours() + 1)[0], convertTo12Hour(d.getHours() + 1)[1]]
-else t = ["half", "past", convertTo12Hour(d.getHours())]
-
-console.log(t)
-
-for (var i = 0; i < t.length; i++)
+function update()
 {
-    var segment = t[i]
+    d = new Date()
 
-    if (i == 0 && typeof segment == "number") {segment = "number_begin_" + segment}
-    if (i == t.length - 2 && typeof segment == "number") {segment = "number_end_" + segment}
+    if (d.getMinutes() >= 58) t = ["", convertTo12Hour(d.getHours() + 1)[0], convertTo12Hour(d.getHours() + 1)[1]]
+    else if (d.getMinutes() <= 2) t = ["", convertTo12Hour(d.getHours())[0], convertTo12Hour(d.getHours())[1]]
+    else if (d.getMinutes() <= 28) t = [findNearest(d.getMinutes()), "minutes", "past", convertTo12Hour(d.getHours())[0], convertTo12Hour(d.getHours())[1]]
+    else if (d.getMinutes() >= 32) t = [findNearest(60 - d.getMinutes()), "minutes", "to", d.getHours == 23 ? "midnight" : convertTo12Hour(d.getHours() + 1)[0], convertTo12Hour(d.getHours() + 1)[1]]
+    else                           t = ["half", "past", convertTo12Hour(d.getHours())]
+
+    // format t
+    for (var i = 0; i < t.length; i++)
+    {        
+        var segment = t[i]
+        if (i == 0 && typeof segment == "number") {segment = "number_begin_" + segment}
+        if (i == t.length - 2 && typeof segment == "number") {segment = "number_end_" + segment}
+    }
 
     for (const element of document.getElementsByClassName("segment"))
     {
-        if (element.getAttribute("data-segment") == segment || (segment == "number_begin_25" && element.getAttribute("data-segment") == "number_begin_20"))
+        if (t.includes(element.getAttribute("data-segment")) || (t.includes("number_begin_25") && element.getAttribute("data-segment") == "number_begin_20"))
         {
             element.classList.add("on")
             element.classList.remove("off")
         }
+        else
+        {
+            element.classList.add("off")
+            element.classList.remove("on")
+        }
     }
+    
+    for (const element of document.getElementsByClassName("alwayson"))
+    {
+        element.classList.add("on")
+        element.classList.remove("off")
+    }
+    
+    if (d.getSeconds() % 2 == 0)
+    {
+        document.getElementById("second_indicator").classList.add("on")
+        document.getElementById("second_indicator").classList.remove("off")
+    }
+    else
+    {
+        document.getElementById("second_indicator").classList.add("off")
+        document.getElementById("second_indicator").classList.remove("on")
+    }
+
+    requestAnimationFrame(update)
 }
+
+requestAnimationFrame(update)
